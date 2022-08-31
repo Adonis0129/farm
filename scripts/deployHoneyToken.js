99689c94abe41e927a4f47b1c5248e0c9df0e39e
Adonis <goldendores9826@gmail.com>
2022-08-31 14:54:03 +0200

    token test

--- scripts/deployHoneyToken.js
+++ scripts/deployHoneyToken.js
@@ -2,6 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 require("dotenv").config();
 
 /**
+ *  _admin = ""
  *  _developmentFounders = 0xC01cbc79644283782BabE262D1C56493d83D6fe2
     _advisors = 0x105F706AB60fcc1F760b1b6cAD331A647272BDCb
     _marketingReservesPool = 0x56edb7B2AB826B64c26C599C050B909c4d8E1a29
@@ -10,7 +11,7 @@ require("dotenv").config();
 
 async function main() {
     const Honey = await ethers.getContractFactory("HoneyToken");
-    const honey = await upgrades.deployProxy(Honey,["HoneyToken", "$HN", 100000, 0xC01cbc79644283782BabE262D1C56493d83D6fe2, 0x105F706AB60fcc1F760b1b6cAD331A647272BDCb, 0x56edb7B2AB826B64c26C599C050B909c4d8E1a29, 0x4962B860e02eb883CB02Bd879641f3d637e123fC]);
+    const honey = await upgrades.deployProxy(Honey,["HoneyToken", "$HN", 100000000000000000000000, "aminAddress", 0xC01cbc79644283782BabE262D1C56493d83D6fe2, 0x105F706AB60fcc1F760b1b6cAD331A647272BDCb, 0x56edb7B2AB826B64c26C599C050B909c4d8E1a29, 0x4962B860e02eb883CB02Bd879641f3d637e123fC]);
     await honey.deployed();
     console.log("HoneyToken proxy deployed to:", honey.address);
 }
